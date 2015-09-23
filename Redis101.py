07008de0225cd43eb00f079b9850e82eac7f1802
Prakhar Gupta <mywolfdale@gmail.com>
2015-09-23 17:23:16 +0530

    Update Redis101.py

--- Redis101.py
+++ Redis101.py
@@ -1,3 +1,14 @@
 #Intro to Redis (NoSql)
 import redis
 
+red = redis.StrictRedis(host='localhost', port=6379, db=0)
+if red.set('Hell','Bound'):
+    print 'All Done !'
+    print red.get('Hell')
+if red.get(raw_input()):
+    print 'Key Exist in DB'
+else:
+    print 'No Key found'
+print 'Flush DB'
+red.flushall()
+
